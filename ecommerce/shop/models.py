b53f9fff5e56b65e7d1ed41eec7c5ff5edfb9d5c
mmendes <matheusmendesbmf@outlook.com>
2024-07-23 10:15:12 -0300

    feat(models):foi criado  a parte do modelo de product, user e order

--- ecommerce/shop/models.py
+++ ecommerce/shop/models.py
@@ -1,3 +1,25 @@
+from urllib.request import AbstractUser
+
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    id_admin = models.BooleanField(default=False)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+
+def __str__(self):
+    return self.name
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
